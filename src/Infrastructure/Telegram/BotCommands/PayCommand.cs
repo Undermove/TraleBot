68caf43c4974689c48b56590675df48c2c39ab14
Dmitry Afonchenko <d.afonchenko@dodopizza.com>
2023-01-20 12:46:43 +0400

    Add some logging to payment

--- src/Infrastructure/Telegram/BotCommands/PayCommand.cs
+++ src/Infrastructure/Telegram/BotCommands/PayCommand.cs
@@ -1,4 +1,5 @@
 using Infrastructure.Telegram.Models;
+using Microsoft.Extensions.Logging;
 using Telegram.Bot;
 using Telegram.Bot.Types.Payments;
 
@@ -8,11 +9,16 @@ public class PayCommand : IBotCommand
 {
     private readonly TelegramBotClient _client;
     private readonly BotConfiguration _configuration;
+    private readonly ILogger _logger;
 
-    public PayCommand(TelegramBotClient client, BotConfiguration configuration)
+    public PayCommand(
+        TelegramBotClient client, 
+        BotConfiguration configuration, 
+        ILoggerFactory logger)
     {
         _client = client;
         _configuration = configuration;
+        _logger = logger.CreateLogger(typeof(PayCommand));
     }
 
     public Task<bool> IsApplicable(TelegramRequest request, CancellationToken ct)
@@ -23,16 +29,26 @@ public class PayCommand : IBotCommand
 
     public async Task Execute(TelegramRequest request, CancellationToken token)
     {
+        _logger.LogInformation("Payment Started");
+        try
+        {
+            await _client.SendInvoiceAsync(
+                request.UserTelegramId,
+                "Payment",
+                "Extended TraleBot",
+                "somePayload",
+                _configuration.PaymentProviderToken,
+                "GEL",
+                new List<LabeledPrice> {new("Premium", 20*100)},
+                cancellationToken: token
+            );
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
         // Send the invoice to the specified chat
-        await _client.SendInvoiceAsync(
-            request.UserTelegramId,
-            "Payment",
-            "Extended TraleBot",
-            "somePayload",
-            _configuration.PaymentProviderToken,
-            "GEL",
-            new List<LabeledPrice> {new("Premium", 20*100)},
-            cancellationToken: token
-        );
+        _logger.LogInformation("Payment Finished");
     }
 }
