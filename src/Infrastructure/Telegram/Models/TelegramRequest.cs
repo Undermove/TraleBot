62e7c70b268feb2885bbb72b24fbd30eafdcbedb
Dmitry Afonchenko <d.afonchenko@dodopizza.com>
2023-01-23 13:02:22 +0400

    Fix bug with incorrect answers when bot receives his own messages

--- src/Infrastructure/Telegram/Models/TelegramRequest.cs
+++ src/Infrastructure/Telegram/Models/TelegramRequest.cs
@@ -18,18 +18,18 @@ public class TelegramRequest
                          ?? request.CallbackQuery?.From.Id 
                          ?? request.MyChatMember?.From.Id 
                          ?? request.PreCheckoutQuery?.From.Id 
-                         ?? throw new ArgumentException();
+                         ?? throw new ArgumentException("User TelegramId not found");
         MessageId = request.Type == UpdateType.PreCheckoutQuery ? 0 : request.Message?.MessageId 
                                                                     ?? request.CallbackQuery?.Message?.MessageId
-                                                                    ?? throw new ArgumentException();
+                                                                    ?? throw new ArgumentException("MessageId not found");
         Text = request.Message?.Text
                ?? request.CallbackQuery?.Data
                ?? request.PreCheckoutQuery?.Id
-               ?? throw new ArgumentException();
+               ?? "/"; // bad hack to skip empty message when bot reacts to his own message
         UserName = request.Message?.Chat.FirstName 
-                   ?? request.CallbackQuery?.From.Username 
-                   ?? request.PreCheckoutQuery?.From.Username 
-                   ?? throw new ArgumentException();
+                   ?? request.CallbackQuery?.From.FirstName 
+                   ?? request.PreCheckoutQuery?.From.FirstName 
+                   ?? throw new ArgumentException("User Name not found");
         UserId = userId;
         RequestType = request.Type;
     }
