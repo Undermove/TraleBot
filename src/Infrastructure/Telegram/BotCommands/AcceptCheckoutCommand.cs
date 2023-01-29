1a5ed1334d5ceac5b37584a5303935dc75373224
Dmitry Afonchenko <d.afonchenko@dodopizza.com>
2023-01-29 14:43:21 +0400

    Remove redundant fields in AcceptCheckoutCommand.cs

--- src/Infrastructure/Telegram/BotCommands/AcceptCheckoutCommand.cs
+++ src/Infrastructure/Telegram/BotCommands/AcceptCheckoutCommand.cs
@@ -1,31 +1,22 @@
 using Application.Invoices;
-using Application.Users.Commands;
 using Infrastructure.Telegram.Models;
 using MediatR;
-using Microsoft.Extensions.Logging;
 using Telegram.Bot;
 using Telegram.Bot.Types.Enums;
-using Telegram.Bot.Types.Payments;
 
 namespace Infrastructure.Telegram.BotCommands;
 
 public class AcceptCheckoutCommand : IBotCommand
 {
     private readonly TelegramBotClient _client;
-    private readonly BotConfiguration _configuration;
-    private readonly ILogger _logger;
     private readonly IMediator _mediator;
 
     public AcceptCheckoutCommand(
-        TelegramBotClient client, 
-        BotConfiguration configuration, 
-        ILoggerFactory logger, 
+        TelegramBotClient client,
         IMediator mediator)
     {
         _client = client;
-        _configuration = configuration;
         _mediator = mediator;
-        _logger = logger.CreateLogger(typeof(PayCommand));
     }
 
     public Task<bool> IsApplicable(TelegramRequest request, CancellationToken ct)
@@ -35,7 +26,7 @@ public class AcceptCheckoutCommand : IBotCommand
 
     public async Task Execute(TelegramRequest request, CancellationToken token)
     {
-        await _mediator.Send(new ProcessPaymentCommand() {UserId = request.UserId}, token);
+        await _mediator.Send(new ProcessPaymentCommand() { UserId = request.UserId, PreCheckoutQueryId = request.Text}, token);
         await _client.AnswerPreCheckoutQueryAsync(request.Text, token);
         await _client.SendTextMessageAsync(
             request.UserTelegramId,
