c594cb2014b769547fb79c2b25b3bfb69935bb05
Dmitry Afonchenko <d.afonchenko@dodopizza.com>
2023-02-20 11:59:00 +0400

    Add count in reverse direction column

---
+++ src/Persistence/Migrations/20230220075435_CountInReverseDirectionColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class CountInReverseDirectionColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SuccessAnswersCountInReverseDirection",
+                table: "VocabularyEntries",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SuccessAnswersCountInReverseDirection",
+                table: "VocabularyEntries");
+        }
+    }
+}
