16362b4b3d085e3a9c51c331851030c1e53a93a2
Dmitry Afonchenko <d.afonchenko@dodopizza.com>
2023-02-21 19:39:33 +0400

    Add alternative quiz result contract

--- src/Application/Quizzes/Commands/CheckQuizAnswer/CheckQuizAnswerResult.cs
+++ src/Application/Quizzes/Commands/CheckQuizAnswer/CheckQuizAnswerResult.cs
@@ -1,3 +1,25 @@
+using Domain.Entities;
+
 namespace Application.Quizzes.Commands.CheckQuizAnswer;
 
 public record CheckQuizAnswerResult(bool IsAnswerCorrect, string CorrectAnswer, int ScoreToNextLevel);
+
+enum WordLevelChanged
+{
+    NothingChanged,
+    MasteredInForwardDirection,
+    MasteredInBothDirections,
+}
+
+public record CheckQuizAnswerResult2(
+    bool IsAnswerCorrect, 
+    int CorrectAnswer, 
+    int ScoreToNextLevel,
+    MasteringLevel NextLevel,
+    QuizDirection QuizDirection);
+
+public enum QuizDirection
+{
+    WordDefinition,
+    DefinitionWord
+}
