3745428c6cf60da91301423c8211f7c13cb56d85
Dmitry Afonchenko <d.afonchenko@dodopizza.com>
2023-03-01 18:08:36 +0400

    Add test project

---
+++ tests/Application.UnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Application.UnitTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
