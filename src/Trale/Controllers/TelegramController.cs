e861ce36062c19e3e9f3c79dae5986de889d5b8c
Dmitry Afonchenko <d.afonchenko@dodopizza.com>
2023-03-07 22:32:07 +0400

    Remove redundant field

--- src/Trale/Controllers/TelegramController.cs
+++ src/Trale/Controllers/TelegramController.cs
@@ -4,7 +4,6 @@ using Application.Common.Interfaces;
 using Infrastructure;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Telegram.Bot;
 using Telegram.Bot.Types;
 
 namespace Trale.Controllers;
@@ -14,18 +13,15 @@ namespace Trale.Controllers;
 public class TelegramController : Controller
 {
     private readonly BotConfiguration _configuration;
-    private readonly TelegramBotClient _telegramBotClient;
     private readonly ILogger _logger;
     private readonly IDialogProcessor _dialogProcessor;
 
     public TelegramController(
         BotConfiguration configuration,
-        TelegramBotClient telegramBotClient,
         ILoggerFactory logger,
         IDialogProcessor dialogProcessor)
     {
         _configuration = configuration;
-        _telegramBotClient = telegramBotClient;
         _dialogProcessor = dialogProcessor;
         _logger = logger.CreateLogger(typeof(TelegramController));
     }
