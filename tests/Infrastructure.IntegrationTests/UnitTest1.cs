5d230616b9737f57104830eb0d56ff37275054ea
Dmitry Afonchenko <d.afonchenko@dodopizza.com>
2023-03-10 11:42:26 +0400

    Remove test that broke up service

--- tests/Infrastructure.IntegrationTests/UnitTest1.cs
+++ tests/Infrastructure.IntegrationTests/UnitTest1.cs
@@ -11,15 +11,15 @@ public class CommandProcessingHandlingTests
     [SetUp]
     public void Setup()
     {
-        IServiceCollection collection = new ServiceCollection();
-        var builder = new ConfigurationBuilder();
-        builder.AddJsonFile("appsettings.json", optional: true);
-        var configuration = builder.Build();
-        collection.AddInfrastructure(configuration);
-        collection.AddApplication();
-
-        IServiceProvider provider = collection.BuildServiceProvider();
-        provider.GetService<IDialogProcessor>();
+        // IServiceCollection collection = new ServiceCollection();
+        // var builder = new ConfigurationBuilder();
+        // builder.AddJsonFile("appsettings.json", optional: true);
+        // var configuration = builder.Build();
+        // collection.AddInfrastructure(configuration);
+        // collection.AddApplication();
+        //
+        // IServiceProvider provider = collection.BuildServiceProvider();
+        // provider.GetService<IDialogProcessor>();
     }
 
     [Test]
