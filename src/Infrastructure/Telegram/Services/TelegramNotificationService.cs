1c5e0d48b8e5e6b1c1e2c67778dfb22a955e41b7
Dmitry Afonchenko <d.afonchenko@dodopizza.com>
2023-03-28 10:56:15 +0400

    Implement notification sender

--- src/Infrastructure/Telegram/Services/TelegramNotificationService.cs
+++ src/Infrastructure/Telegram/Services/TelegramNotificationService.cs
@@ -1,6 +1,5 @@
 using Application.Common.Interfaces;
 using Domain.Entities;
-using MediatR;
 using Telegram.Bot;
 
 namespace Infrastructure.Telegram.Services;
@@ -8,18 +7,16 @@ namespace Infrastructure.Telegram.Services;
 public class TelegramNotificationService: IUserNotificationService
 {
     private readonly TelegramBotClient _client;
-    private readonly IMediator _mediator;
 
-    public TelegramNotificationService(TelegramBotClient client, IMediator mediator)
+    public TelegramNotificationService(TelegramBotClient client)
     {
         _client = client;
-        _mediator = mediator;
     }
 
     public async Task NotifyAboutUnlockedAchievementAsync(Achievement achievement, CancellationToken ct)
     {
         await _client.SendTextMessageAsync(
-            0,
+            achievement.User.TelegramId,
             "✅Платеж принят. Спасибо за поддержку нашего бота! Вам доступны дополнительные фичи.",
             cancellationToken: ct);
     }
