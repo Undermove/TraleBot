89b709fdf376dbd968ec9dc75c060713707ed6f2
Dmitry Afonchenko <d.afonchenko@dodopizza.com>
2023-04-05 21:37:30 +0400

    Add AmethystChecker.cs

---
+++ src/Application/Achievements/Services/Checkers/AmethystChecker.cs
@@ -0,0 +1,17 @@
+using Application.Achievements.Services.Triggers;
+
+namespace Application.Achievements.Services.Checkers;
+
+public class AmethystChecker: IAchievementChecker<WordMasteringLevelTrigger>
+{
+    public string Icon => "🏵";
+    public string Name => "Аметист";
+    public string Description => "10 слов с бриллиантом в словаре";
+    public Guid AchievementTypeId => Guid.Parse("ABD909BB-926C-4F73-8AF9-61286468F6AB");
+
+    public bool CheckAchievement(object trigger)
+    {
+        var vocabularyEntry = trigger as WordMasteringLevelTrigger;
+        return vocabularyEntry is { BrilliantWordsCount: >= 10 };
+    }
+}
