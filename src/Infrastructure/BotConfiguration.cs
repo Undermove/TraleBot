5b7951f6761dea330900837c9c23a32662d77050
Dmitry Afonchenko <d.afonchenko@dodopizza.com>
2023-05-27 12:54:33 +0400

    Remove startup and move all to program.cs

--- src/Infrastructure/BotConfiguration.cs
+++ src/Infrastructure/BotConfiguration.cs
@@ -2,6 +2,7 @@
 
 public class BotConfiguration
 {
+    public const string Configuration = "BotConfiguration"; 
     public required string Token { get; init; }
     public required string HostAddress { get; init; }
     public required string WebhookToken { get; init; }
--- src/Trale/Program.cs
+++ src/Trale/Program.cs
@@ -1,63 +1,61 @@
 using System;
-using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+using Application;
+using Infrastructure;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Persistence;
-using Serilog;
-using Serilog.Sinks.Loki;
+using Trale.HostedServices;
 
-namespace Trale;
+var hostBuilder = WebApplication.CreateBuilder(args);
 
-public class Program
+// Setup Bot configuration
+const string aspNetCoreEnvironment = "ASPNETCORE_ENVIRONMENT";
+string environmentName = Environment.GetEnvironmentVariable(aspNetCoreEnvironment);
+
+hostBuilder.WebHost.ConfigureAppConfiguration((_, config) =>
 {
-    public static async Task Main(string[] args)
+    config.AddJsonFile("appsettings.json")
+        .AddJsonFile($"appsettings.{environmentName}.json", optional: true)
+        .AddEnvironmentVariables();
+});
+
+var configuration = hostBuilder.Configuration;
+
+hostBuilder.Services
+    .AddControllers()
+    .AddJsonOptions(options=>
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()))
+    .AddNewtonsoftJson();
+
+hostBuilder.Services.AddApplication();
+hostBuilder.Services.AddPersistence(configuration);
+        
+hostBuilder.Services.AddInfrastructure(configuration);
+hostBuilder.Services.AddHostedService<CreateWebhook>();
+
+hostBuilder.WebHost.UseUrls("http://*:1402/");
+var host = hostBuilder.Build();
+
+using (var scope = host.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    try
     {
-        string AspNetCoreEnvironment = "ASPNETCORE_ENVIRONMENT";
-        string environmentName = Environment.GetEnvironmentVariable(AspNetCoreEnvironment);
-        var host = CreateHostBuilder(args)
-            .ConfigureAppConfiguration((_, config) =>
-            {
-                config.AddJsonFile("appsettings.json")
-                    .AddJsonFile($"appsettings.{environmentName}.json", optional: true)
-                    .AddEnvironmentVariables();
-            })
-            .UseSerilog((context, configuration) =>
-            {
-                configuration
-                    .ReadFrom.Configuration(context.Configuration)
-                    .WriteTo.LokiHttp("http://localhost:3031");
-            })
-            .Build();
-        
-        using (var scope = host.Services.CreateScope())
-        {
-            var services = scope.ServiceProvider;
-
-            try
-            {
-                var botDbContext = services.GetRequiredService<TraleDbContext>();
-                await botDbContext.Database.MigrateAsync();
-            }
-            catch (Exception ex)
-            {
-                var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
-                logger.LogError(ex, "An error occurred while migrating or initializing the database.");
-            }
-        }
-        
-        await host.RunAsync();
+        var botDbContext = services.GetRequiredService<TraleDbContext>();
+        await botDbContext.Database.MigrateAsync();
     }
-
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder
-                    .UseUrls("http://*:1402/")
-                    .UseStartup<Startup>();
-            });
+    catch (Exception ex)
+    {
+        var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while migrating or initializing the database.");
+    }
 }
+
+host.MapControllers();
+await host.RunAsync();
