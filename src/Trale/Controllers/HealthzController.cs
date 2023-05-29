bbead736e4833173397f652b574bcce253cead41
Dmitry Afonchenko <d.afonchenko@dodopizza.com>
2023-05-29 13:59:38 +0400

    Remove test code

--- src/Trale/Controllers/HealthzController.cs
+++ src/Trale/Controllers/HealthzController.cs
@@ -6,23 +6,9 @@ namespace Trale.Controllers;
 [Route("healthz")]
 public class HealthzController : Controller
 {
-    private readonly string _myValue;
-
-    public HealthzController(IConfiguration config)
-    {
-        _myValue = config.GetConnectionString("Test");
-    }
-    
     [HttpGet]
     public IActionResult Get()
     {
         return Ok();
-    }
-    
-    [HttpGet]
-    [Route("check")]
-    public IActionResult GetCheck()
-    {
-        return Ok(_myValue);
     }
 }
