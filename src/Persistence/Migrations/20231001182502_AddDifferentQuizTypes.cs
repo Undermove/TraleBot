a9e6e5101a46c51f5ce1d60280de11f66f5bde03
Dmitry Afonchenko <d.afonchenko@dodopizza.com>
2023-10-02 09:59:46 +0400

    Add migrations for questions with variants

---
+++ src/Persistence/Migrations/20231001182502_AddDifferentQuizTypes.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDifferentQuizTypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "QuestionType",
+                table: "QuizQuestions",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Variants",
+                table: "QuizQuestions",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "QuestionType",
+                table: "QuizQuestions");
+
+            migrationBuilder.DropColumn(
+                name: "Variants",
+                table: "QuizQuestions");
+        }
+    }
+}
