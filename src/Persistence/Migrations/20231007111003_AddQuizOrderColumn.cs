1fa73dec0bd247cff2103c32350453ec2426b9ed
Dmitry Afonchenko <d.afonchenko@dodopizza.com>
2023-10-07 15:10:56 +0400

    Add order in quiz column migration

---
+++ src/Persistence/Migrations/20231007111003_AddQuizOrderColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddQuizOrderColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "OrderInQuiz",
+                table: "QuizQuestions",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OrderInQuiz",
+                table: "QuizQuestions");
+        }
+    }
+}
