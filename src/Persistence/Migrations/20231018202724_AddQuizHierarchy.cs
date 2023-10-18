48201e6f13e21c342ee1fc56bbbf27e92d8e7ba8
Dmitry Afonchenko <d.afonchenko@dodopizza.com>
2023-10-19 00:28:18 +0400

    Add migration for shared quiz

---
+++ src/Persistence/Migrations/20231018202724_AddQuizHierarchy.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddQuizHierarchy : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedByUserName",
+                table: "Quizzes",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "CreatedByUserScore",
+                table: "Quizzes",
+                type: "double precision",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "QuizType",
+                table: "Quizzes",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedByUserName",
+                table: "Quizzes");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedByUserScore",
+                table: "Quizzes");
+
+            migrationBuilder.DropColumn(
+                name: "QuizType",
+                table: "Quizzes");
+        }
+    }
+}
