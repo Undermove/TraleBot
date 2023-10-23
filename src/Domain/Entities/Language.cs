cb10c9b4dd6323857a487e7eae8c14c897e4d79a
Dmitry Afonchenko <d.afonchenko@dodopizza.com>
2023-10-23 11:21:01 +0400

    Add language for current settings

---
+++ src/Domain/Entities/Language.cs
@@ -0,0 +1,7 @@
+namespace Domain.Entities;
+
+public enum Language
+{
+    English,
+    Georgian
+}
--- src/Domain/Entities/UserSettings.cs
+++ src/Domain/Entities/UserSettings.cs
@@ -4,4 +4,5 @@ public class UserSettings
 {
     public Guid UserId { get; set; }
     public virtual User User { get; set; }
+    public Language CurrentLanguage { get; set; }
 }
