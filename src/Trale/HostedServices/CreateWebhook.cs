3d53dfa88bf789b2c26871a90fc46d2183e75b05
Dmitry Afonchenko <d.afonchenko@dodopizza.com>
2023-12-15 00:37:05 +0400

    Add logging

--- src/Trale/HostedServices/CreateWebhook.cs
+++ src/Trale/HostedServices/CreateWebhook.cs
@@ -1,7 +1,9 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using Infrastructure.Telegram;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Telegram.Bot;
 
 namespace Trale.HostedServices;
@@ -10,18 +12,27 @@ public class CreateWebhook : IHostedService
 {
     private readonly BotConfiguration _config;
     private readonly ITelegramBotClient _telegramBotClient;
+    private readonly ILogger<CreateWebhook> _logger;
 
-    public CreateWebhook(BotConfiguration config, ITelegramBotClient telegramBotClient)
+    public CreateWebhook(BotConfiguration config, ITelegramBotClient telegramBotClient, ILogger<CreateWebhook> logger)
     {
         _config = config;
         _telegramBotClient = telegramBotClient;
+        _logger = logger;
     }
         
     public async Task StartAsync(CancellationToken cancellationToken)
     {
-        if (!string.IsNullOrEmpty(_config.HostAddress))
+        try
         {
-            await _telegramBotClient.SetWebhookAsync($"{_config.HostAddress}/telegram/{_config.WebhookToken}", dropPendingUpdates: false, cancellationToken: cancellationToken);
+            if (!string.IsNullOrEmpty(_config.HostAddress))
+            {
+                await _telegramBotClient.SetWebhookAsync($"{_config.HostAddress}/telegram/{_config.WebhookToken}", dropPendingUpdates: false, cancellationToken: cancellationToken);
+            }
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Error while creating webhook for bot {BotName}", _config.BotName);
         }
     }
 
