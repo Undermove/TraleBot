6ded1bfd174494f81196a849161d3d100308cd74
Dmitry Afonchenko <d.afonchenko@dodopizza.com>
2023-12-15 13:04:09 +0400

    Remove temp logging

--- src/Trale/HostedServices/CreateWebhook.cs
+++ src/Trale/HostedServices/CreateWebhook.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
 using Infrastructure.Telegram;
 using Microsoft.Extensions.Hosting;
@@ -12,29 +11,20 @@ public class CreateWebhook : IHostedService
 {
     private readonly BotConfiguration _config;
     private readonly ITelegramBotClient _telegramBotClient;
-    private readonly ILogger<CreateWebhook> _logger;
 
     public CreateWebhook(BotConfiguration config, ITelegramBotClient telegramBotClient, ILogger<CreateWebhook> logger)
     {
         _config = config;
         _telegramBotClient = telegramBotClient;
-        _logger = logger;
     }
         
     public async Task StartAsync(CancellationToken cancellationToken)
     {
-        try
+        if (!string.IsNullOrEmpty(_config.HostAddress))
         {
-            if (!string.IsNullOrEmpty(_config.HostAddress))
-            {
-                await _telegramBotClient.SetWebhookAsync($"{_config.HostAddress}/telegram/{_config.WebhookToken}", 
-                    dropPendingUpdates: false, 
-                    cancellationToken: cancellationToken);
-            }
-        }
-        catch (Exception e)
-        {
-            _logger.LogError(e, "Error while creating webhook for bot {BotName}", _config.BotName);
+            await _telegramBotClient.SetWebhookAsync($"{_config.HostAddress}/telegram/{_config.WebhookToken}", 
+                dropPendingUpdates: false, 
+                cancellationToken: cancellationToken);
         }
     }
 
