cd8b3b16fb862b66807afe8bb842e0be47c98d8e
Dmitry Afonchenko <d.afonchenko@dodopizza.com>
2024-02-12 13:11:41 +0400

    Add users is active field

---
+++ src/Persistence/Migrations/20240212091020_UserIsActive.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserIsActive : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "QuizType",
+                table: "Quizzes",
+                type: "character varying(13)",
+                maxLength: 13,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "QuestionType",
+                table: "QuizQuestions",
+                type: "character varying(34)",
+                maxLength: 34,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "QuizType",
+                table: "Quizzes",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(13)",
+                oldMaxLength: 13);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "QuestionType",
+                table: "QuizQuestions",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(34)",
+                oldMaxLength: 34);
+        }
+    }
+}
