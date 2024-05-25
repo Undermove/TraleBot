9ee0513984445d7db66653e51dd4139a1c81e085
Dmitry Afonchenko <d.afonchenko@dodopizza.com>
2024-05-25 11:39:47 +0400

    Add slider customization

--- scripts.js
+++ scripts.js
@@ -4,6 +4,8 @@ $(document).ready(function(){
         infinite: true,
         speed: 300,
         slidesToShow: 1,
+        centerMode: true,
+        variableWidth: true,
         adaptiveHeight: true
     });
 });
