5227e7125e1e39cf869a84173f12fcd228d23e9f
Dmitry Afonchenko <d.afonchenko@dodopizza.com>
2024-05-25 11:30:16 +0400

    Add carousel

--- scripts.js
+++ scripts.js
@@ -1,17 +1,9 @@
-let slideIndex = 0;
-showSlides();
-
-function plusSlides(n) {
-    slideIndex += n;
-    showSlides();
-}
-
-function showSlides() {
-    let slides = document.getElementsByClassName("carousel-slide");
-    if (slideIndex >= slides.length) { slideIndex = 0 }
-    if (slideIndex < 0) { slideIndex = slides.length - 1 }
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[slideIndex].style.display = "block";
-}
+$(document).ready(function(){
+    $('.screenshots').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true
+    });
+});
